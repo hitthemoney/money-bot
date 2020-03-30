b504d6cadf11b4cdfc8f992f5fe7f06613811481
hitthemoney <asher@grusd.com>
2020-03-30 15:46:40 -0700

    Update

--- moneyBot py/bot.py
+++ moneyBot py/bot.py
@@ -1,7 +1,8 @@
 import discord
 from discord.ext import commands
+import msgpack
 
-TOKEN = 
+TOKEN = ''
 
 prefix1 = 'money '
 
@@ -28,13 +29,19 @@ async def help(ctx):
 Commands:
 - money help; brings you to this page
 - money warn <user> <reason>; warns a user (only works if you can kick members)
-- money kick <user> <reason>; warns a user (only works if you can kick members)
+- money kick <user> <reason>; kicks a user (only works if you can kick members)
+- money ban <user> <reason>; bans a user (only works if you can ban members)
+- money unban <user id> <bool (True reinvites the member and False does not)>; unbans a user (only works if you can ban members)
+- money mute <user> <reason>; makes a user unable to chat (only works if you can kick members, and have setup a role called "muted" (case sensitive))
+- money unmute <user> <reason>; unmutes a muted user (only works if you can kick members, and have the "muted" role)
+- money invite <id>; invites a user
 - money yt; my YouTube channel
 - money twitch; my Twitch channel
 - money website; my website
 - money bot_info; the bot information including invite link
+- money server_info; the server info (Note this command is currently broken)
 
-WORKING ON: money mute <user> and money server_info```""")
+WORKING ON: fixing money server_info```""")
 
 @client.command(aliases=['youtube', 'YT', 'Youtube', 'Yt', 'yT', 'YouTube', 'YOUTUBE'])
 async def yt(ctx):
@@ -48,26 +55,86 @@ async def website(ctx):
 
 @client.command(pass_context = True, aliases=['WARN', 'Warn',' warn', 'warN', 'WARNN', 'Warnn', 'warnn'])
 @commands.has_permissions(kick_members = True)
-async def warn(ctx, name, reason):
-    await ctx.send("""You have been warned {}. 
-Reason: {}""".format(name, reason))
+async def warn(ctx, name: discord.Member, reason):
+    await ctx.send("""**You have been warned {}. 
+Reason: {}**""".format(name, reason))
+    guild = client.get_guild(ctx.guild.id)
+    channel = await name.create_dm()
+    await channel.send("""**You were warned from {}
+Reason: {}**""".format(guild, reason))
 
 @client.command(pass_context = True, aliases=['Kick','KICK','KICKK','Kickk','kickk'])
 @commands.has_permissions(kick_members = True)
 async def kick(ctx, name: discord.Member, *, reason = None):
     await name.kick(reason = reason)
-    await ctx.send("""{} was kicked for {}""".format(name, reason))
+    await ctx.send("""**{} was kicked
+Reason: {}**""".format(name, reason))
+    guild = client.get_guild(ctx.guild.id)
+    channel = await name.create_dm()
+    await channel.send("""**You were kicked from {}
+Reason: {}**""".format(guild, reason))
 
-@client.command(pass_context = True)
+@client.command(pass_context = True, aliases=['Ban','BAN','Bann','bann','BANN'])
+@commands.has_permissions(ban_members = True)
+async def ban(ctx, name: discord.Member, *, reason = None):
+    await name.ban(reason = reason)
+    await ctx.send("""**{} was banned
+Reason: {}**""".format(name, reason))
+    guild = client.get_guild(ctx.guild.id)
+    channel = await name.create_dm()
+    await channel.send("""**You were banned from {}
+Reason: {}**""".format(guild, reason))
+
+@client.command(pass_context = True, aliases=['Unban','UNBAN','Unbann','unbann','UNBANN'])
+@commands.has_permissions(ban_members = True)
+async def unban(ctx, id: int, invite: bool):
+    name = await client.fetch_user(id)
+    await ctx.guild.unban(name)
+    await ctx.send("""**{} was unbanned**""".format(name))
+    if invite == True:
+        messsageAuthor = ctx.message.author
+        link = await ctx.channel.create_invite(max_age = None)
+        guild = client.get_guild(ctx.guild.id)
+        channel = await name.create_dm()
+        await channel.send("""**You were unbanned from {}
+{} would like to invite you back {}**""".format(guild, messsageAuthor, link))
+    else:
+        guild = client.get_guild(ctx.guild.id)
+        channel = await name.create_dm()
+        await channel.send("""**You were unbanned from {}**""".format(guild))
+
+@client.command(pass_context=True, aliases=['MUTE', 'Mute', 'mutee'])
 @commands.has_permissions(kick_members = True)
-async def mute(ctx, name: discord.User, reason1):
-    author = ctx.message.author
-    await ctx.send("""{} was muted by {}. 
-Reason: {}""".format(name, author, reason1))
-    await ctx.send_message("""You were muted {}. 
-Reason: {}""".format(name, reason1))
-    #await client.add_roles(*"muted", reason = reason1, atomic = True)
+async def mute(ctx, name: discord.Member, reason):
+    test = discord.utils.get(name.guild.roles, name="muted")
+    await discord.Member.add_roles(name, test)
+    await ctx.send("""**{} was muted
+Reason: {}**""".format(name, reason))
+    guild = client.get_guild(ctx.guild.id)
+    channel = await name.create_dm()
+    await channel.send("""**You were muted from {}
+Reason: {}**""".format(guild, reason))
 
+@client.command(pass_context=True, aliases=['UNMUTE', 'Unmute', 'unmutee'])
+@commands.has_permissions(kick_members = True)
+async def unmute(ctx, name: discord.Member):
+    test = discord.utils.get(name.guild.roles, name="muted")
+    await discord.Member.remove_roles(name, test)
+    await ctx.send("""**{} was unmuted**""".format(name))
+    guild = client.get_guild(ctx.guild.id)
+    channel = await name.create_dm()
+    await channel.send("""**You were unmuted from {}**""".format(guild))
+
+@client.command(aliases=['Invite','INVITE','INVITEE','Invitee','invitee'])
+async def invite(ctx, id: int):
+    messsageAuthor = ctx.message.author
+    name = await client.fetch_user(id)
+    await ctx.send("""**{} sucessfully invited {}**""".format(messsageAuthor, name))
+    link = await ctx.channel.create_invite(max_age = None)
+    guild = client.get_guild(ctx.guild.id)
+    channel = await name.create_dm()
+    await channel.send("""**{} would like to invite you to {}**
+{}""".format(messsageAuthor, guild, link))
 
 @client.command()
 async def bot_info(ctx):
@@ -89,22 +156,13 @@ async def server_info(ctx):
         color = discord.Color.gold(),
         title = "Server Info",
     )
-    server_info_embed.set_thumbnail(url = discord.Guild.icon_url)
-    #server_info_embed.add_field(name = "Name", value = discord.Guild.name)
-    server_info_embed.add_field(name = "Member Count", value = discord.Guild.member_count)
-    server_info_embed.add_field(name = "Creation Date", value = discord.Guild.created_at)
+    guildName = client.get_guild(ctx.guild.id)
+    #server_info_embed.set_thumbnail(url = discord.Guild.icon_url) 
+    server_info_embed.add_field(name = "Name", value = guildName)
+    server_info_embed.add_field(name = "Member Count", value = 'Currently Broken' ) #discord.Guild.member_count
+    server_info_embed.add_field(name = "Creation Date", value = 'Currently Broken' ) #discord.Guild.created_at
     server_info_embed.set_footer(text = "Command: money server_info")
 
     await ctx.send(embed=server_info_embed)
 
-#@client.command(pass_context = True)
-#@commands.has_permissions(administrator = True)
-#async def change_prefix(ctx, prefix1):
-    #global client
-    #client = commands.Bot(command_prefix = [prefix1])
-    #await ctx.send('The server prefix has been changed to "{}"'.format(prefix1))
-    #return client
-
 client.run(TOKEN) 
-
-#discord.Guild.icon_url
